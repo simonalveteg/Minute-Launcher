d65ec2194d120b984b090f8c3fb583803dec796e
= <simon.alveteg@gmail.com>
2022-07-31 13:33:54 +0200

    Added TextField to the top of AppList

--- app/src/main/java/com/example/android/minutelauncher/AppList.kt
+++ app/src/main/java/com/example/android/minutelauncher/AppList.kt
@@ -7,21 +7,23 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppList(
     listState: LazyListState,
@@ -44,22 +46,35 @@ fun AppList(
     }
 
     Surface {
-        LazyColumn(
-            state = listState,
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            item {
-                Spacer(Modifier.windowInsetsTopHeight(WindowInsets.statusBars))
+        Column {
+            Row(
+                Modifier
+                    .statusBarsPadding()) {
+                TextField(
+                    value = "test",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                    colors = TextFieldDefaults.outlinedTextFieldColors()
+                )
             }
-            items(installedPackages) { app ->
-                Row {
-                    val appTitle by viewModel.getAppTitle(app)
-                    val appUsage by viewModel.getUsageForApp(app.activityInfo.packageName)
-                    AppCard(appTitle, appUsage) { viewModel.onEvent(Event.OpenApplication(app)) }
+            LazyColumn(
+                state = listState,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                item {
+                    Spacer(modifier = Modifier.height(24.dp))
+                }
+                items(installedPackages) { app ->
+                    Row {
+                        val appTitle by viewModel.getAppTitle(app)
+                        val appUsage by viewModel.getUsageForApp(app.activityInfo.packageName)
+                        AppCard(appTitle, appUsage) { viewModel.onEvent(Event.OpenApplication(app)) }
+                    }
                 }
             }
         }
