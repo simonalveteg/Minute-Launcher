aa3ed830286ae6fed040cdf21bc3f59c780d1be7
= <simon.alveteg@gmail.com>
2022-08-06 16:43:35 +0200

    Minor formatting improvement of SettingsScreen

--- app/src/main/java/com/example/android/minutelauncher/SettingsScreen.kt
+++ app/src/main/java/com/example/android/minutelauncher/SettingsScreen.kt
@@ -1,16 +1,22 @@
 package com.example.android.minutelauncher
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SettingsScreen() {
   Surface {
-    Column(Modifier.fillMaxSize()) {
+    Column(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
       Text(text = "Gestures")
       Text(text = "Upper")
       Text(text = "Lower")
