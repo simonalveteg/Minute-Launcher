5a64168a3837330d1ba0735795a5799334ba4079
= <simon.alveteg@gmail.com>
2022-08-07 16:12:07 +0200

    Added logging

--- app/src/main/java/com/example/android/minutelauncher/MainScreen.kt
+++ app/src/main/java/com/example/android/minutelauncher/MainScreen.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.launch
 
@@ -17,7 +17,7 @@ fun MainScreen(
   viewModel: LauncherViewModel = hiltViewModel()
 ) {
   val mContext = LocalContext.current
-  val bottomSheetExpanded = remember { mutableStateOf(false) }
+  val bottomSheetExpanded = rememberSaveable { mutableStateOf(false) }
   val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
     bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed) {
       Log.d("MAIN_SCREEN", it.name)
@@ -31,6 +31,7 @@ fun MainScreen(
   var openDialogApp by remember { mutableStateOf<UserApp?>(null) }
 
   LaunchedEffect(key1 = true) {
+    Log.d("LAUNCHED_EFFECT","Effect launched. $bottomSheetScaffoldState")
     viewModel.uiEvent.collect { event ->
       Log.d("MAIN_SCREEN", "event: $event")
       when (event) {
@@ -48,7 +49,11 @@ fun MainScreen(
           launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
         }
         is UiEvent.ShowAppsList -> {
-          launch { bottomSheetScaffoldState.bottomSheetState.expand() }
+          launch {
+            Log.d("MAIN_SCREEN","expand sheet $bottomSheetScaffoldState")
+            bottomSheetScaffoldState.bottomSheetState.expand()
+            Log.d("MAIN_SCREEN","sheet expanded $bottomSheetScaffoldState")
+          }
         }
         is UiEvent.ShowAppInfo -> {
           openDialogApp = event.app
@@ -63,7 +68,7 @@ fun MainScreen(
 
   BottomSheetScaffold(
     scaffoldState = bottomSheetScaffoldState,
-    sheetPeekHeight = 0.dp,
+    //sheetPeekHeight = 0.dp,
     sheetContent = {
       if (openDialogApp != null) {
         AppInfo(
