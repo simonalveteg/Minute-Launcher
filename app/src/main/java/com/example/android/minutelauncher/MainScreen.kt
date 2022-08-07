14c0c585b9e8d6ae7f470c0e302ecf45eef78b76
= <simon.alveteg@gmail.com>
2022-08-08 01:06:10 +0200

    Bug: search not getting focus after pressing back button
    Now gets focus, but focus is cleared when scrolling down and letting the
    list stop scrolling. Might be nice?

--- app/src/main/java/com/example/android/minutelauncher/MainScreen.kt
+++ app/src/main/java/com/example/android/minutelauncher/MainScreen.kt
@@ -72,6 +72,7 @@ fun MainScreen(
         onBackPressed = {
           coroutineScope.launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
           viewModel.onEvent(Event.UpdateSearch(""))
+          bottomSheetExpanded.value = false
         }
       )
     },
