33623bbfdadb11ff04c27617e16d385c41503f9d
= <simon.alveteg@gmail.com>
2023-11-01 00:00:31 +0100

    Fix bug where gesture app couldn't be set

--- app/src/main/java/com/example/android/minutelauncher/db/LauncherRepository.kt
+++ app/src/main/java/com/example/android/minutelauncher/db/LauncherRepository.kt
@@ -1,11 +1,7 @@
 package com.example.android.minutelauncher.db
 
 import com.example.android.minutelauncher.GestureDirection
-import kotlinx.coroutines.flow.forEach
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.flow.transform
-import kotlinx.coroutines.flow.transformLatest
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -18,7 +14,10 @@ class LauncherRepository @Inject constructor(private val dao: LauncherDAO) {
   fun favoriteApps() = dao.getFavoriteApps()
   fun insertApp(app: App) = dao.insertApp(app)
   fun toggleFavorite(app: App) = dao.toggleFavoriteApp(app)
-  fun insertGestureApp(swipeApp: SwipeApp) = dao.insertGestureApp(swipeApp)
+  fun insertGestureApp(swipeApp: SwipeApp) {
+    dao.removeAppForGesture(swipeApp.swipeDirection.toString())
+    dao.insertGestureApp(swipeApp)
+  }
   fun removeAppForGesture(gesture: GestureDirection) = dao.removeAppForGesture(gesture.toString())
   fun getAppForGesture(gesture: GestureDirection) = dao.getAppForGesture(gesture.toString())
   fun updateFavoritesOrder(new: List<FavoriteAppWithApp>) {
