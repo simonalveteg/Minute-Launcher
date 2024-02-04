eb52858ecba316fb5f241efd2240e25a0b237bd7
= <simon.alveteg@gmail.com>
2024-02-04 18:26:21 +0100

    Move Timber init from activity to application

--- app/src/main/java/com/example/android/minutelauncher/MainActivity.kt
+++ app/src/main/java/com/example/android/minutelauncher/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    Timber.plant(Timber.DebugTree())
     setContent {
       MinuteLauncherTheme {
         val navController = rememberNavController()
--- app/src/main/java/com/example/android/minutelauncher/MinuteLauncher.kt
+++ app/src/main/java/com/example/android/minutelauncher/MinuteLauncher.kt
@@ -2,8 +2,13 @@ package com.example.android.minutelauncher
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 
 @HiltAndroidApp
 class MinuteLauncher : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    Timber.plant(Timber.DebugTree())
+  }
 }
