bc257e2f2e1cd09cab50c9d8a0a028d9433f2593
= <simon.alveteg@gmail.com>
2024-03-21 22:30:58 +0100

    Add cards for each gesture

--- app/src/main/java/com/alveteg/simon/minutelauncher/settings/GestureScreen.kt
+++ app/src/main/java/com/alveteg/simon/minutelauncher/settings/GestureScreen.kt
@@ -1,15 +1,19 @@
 package com.alveteg.simon.minutelauncher.settings
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
@@ -17,25 +21,30 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.alveteg.simon.minutelauncher.data.App
 import com.alveteg.simon.minutelauncher.data.LauncherViewModel
 import com.alveteg.simon.minutelauncher.theme.archivoBlackFamily
+import com.alveteg.simon.minutelauncher.theme.archivoFamily
+import com.alveteg.simon.minutelauncher.utilities.Gesture
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun GestureScreen(
   navController: NavController,
   viewModel: LauncherViewModel = hiltViewModel()
 ) {
   val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+  val gestureApps by viewModel.gestureApps.collectAsState(initial = emptyMap())
 
   Surface(
     modifier = Modifier.fillMaxSize(),
@@ -69,8 +78,45 @@ fun GestureScreen(
           .padding(paddingValues)
           .padding(horizontal = 16.dp)
       ) {
-        items(20) {
-          GestureEntry(name = "Swipe left on upper half of screen")
+        stickyHeader {
+          Text(
+            text = "UPPER HALF OF SCREEN",
+            fontFamily = archivoFamily,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.padding(top = 4.dp)
+          )
+        }
+        item {
+          GestureEntry(
+            name = "Swipe Left",
+            gesture = Gesture.UPPER_LEFT,
+            app = gestureApps[Gesture.UPPER_LEFT]
+          )
+          GestureEntry(
+            name = "Swipe Right",
+            gesture = Gesture.UPPER_RIGHT,
+            app = gestureApps[Gesture.UPPER_RIGHT]
+          )
+        }
+        stickyHeader {
+          Text(
+            text = "LOWER HALF OF SCREEN",
+            fontFamily = archivoFamily,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.padding(top = 4.dp)
+          )
+        }
+        item {
+          GestureEntry(
+            name = "Swipe Left",
+            gesture = Gesture.LOWER_LEFT,
+            app = gestureApps[Gesture.LOWER_LEFT]
+          )
+          GestureEntry(
+            name = "Swipe Right",
+            gesture = Gesture.LOWER_RIGHT,
+            app = gestureApps[Gesture.LOWER_RIGHT]
+          )
         }
       }
     }
@@ -79,12 +125,44 @@ fun GestureScreen(
 
 @Composable
 fun GestureEntry(
-  name: String
+  name: String,
+  gesture: Gesture,
+  app: App?
 ) {
-  Column {
-    Text(text = name)
-    Row {
-
+  val appTitle = app?.appTitle ?: "No app selected"
+  Surface(
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+    tonalElevation = 4.dp,
+    shape = MaterialTheme.shapes.medium
+  ) {
+    Column(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 8.dp)
+        .padding(start = 16.dp, end = 8.dp)
+    ) {
+      Text(
+        text = name,
+        fontFamily = archivoFamily
+      )
+      Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+      ) {
+        FilledTonalButton(
+          onClick = { /*TODO*/ },
+          modifier = Modifier.weight(1f),
+          shape = MaterialTheme.shapes.medium
+        ) {
+          Text(text = appTitle)
+        }
+        IconButton(
+          onClick = { /*TODO*/ },
+          modifier = Modifier.wrapContentWidth()
+        ) {
+          Icon(imageVector = Icons.Default.Close, contentDescription = "Unset Gesture")
+        }
+      }
     }
   }
 }
